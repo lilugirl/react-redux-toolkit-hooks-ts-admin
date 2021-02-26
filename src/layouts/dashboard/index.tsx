b64330c50471ed4246ab7b43ca7488b23367e0bf
liu yi <lilugirl2005@126.com>
2021-02-26 19:17:14 +0800

    feat:LAYOUT

--- src/layouts/dashboard/index.tsx
+++ src/layouts/dashboard/index.tsx
@@ -1,17 +1,64 @@
-import Header  from './header';
-import Footer from './footer';
 import RouterView from 'core/router';
-
-import React from 'react';
-const Layout=(props:any)=>{
-    console.log('dashboard props',props)
+import Header from './header';
+import React, { useState } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import './style.scss';
+const DashboardLayout=(props:any)=>{
+    const {  Content, Footer, Sider } = Layout;
+    const { SubMenu } = Menu;
+    const [collapsed,setCollapsed]=useState(false);
+    const onCollapse=()=>{
+        setCollapsed(!collapsed)
+    }
     return (
-        <div>
-            <Header />
-              <RouterView routes={props.routes}></RouterView>
-           <Footer />
-        </div>
+
+        <Layout style={{ minHeight: '100vh' }} >
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+         <Header />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                <RouterView routes={props.routes}></RouterView>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+       
     )
 }
 
-export default Layout;
+export default DashboardLayout;
