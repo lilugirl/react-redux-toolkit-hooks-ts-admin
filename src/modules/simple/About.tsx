711b731d341ff572aa88a083009e41aba32e1f92
liu yi <lilugirl2005@126.com>
2021-03-07 15:18:01 +0800

    react hook form的简单用法

--- src/modules/simple/About.tsx
+++ src/modules/simple/About.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 const About=()=>{
-    return (<div>About</div>)
+    const {register,handleSubmit,errors}=useForm();
+    const onSubmit=(data:any)=>{
+      console.log(data)
+    }
+    return (<div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="firstname" placeholder="first name" ref={register} />
+            <input name="lastname" placeholder="last name" ref={register({required:true})} />
+            {errors.lastname && 'Last name is required.'}
+            <input name="age" placeholder="age" ref={register({pattern: /\d+/})} />
+            {errors.age && 'Please enternumber for age.'}
+            <input type="submit" />
+        </form>
+
+    </div>)
 }
 
 export default About;
